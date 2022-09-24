2846208a665569eb1632efc77e28ce4bedf2ac39
npv2k1 <73846954+npv2k1@users.noreply.github.com>
2022-09-24 19:56:03 +0000

    Deploying to gh-pages from @ nvnio-lab/hello-nextjs@2b1f12726d8c240cc897669b9596cf4e634f4610 🚀

--- _next/static/4u1sManXgCsJ8DpkDDbT4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-983383c9289e8f8e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
