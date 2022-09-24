8563b1744ec6fb3e14f5d728e5ed9cdcef25d6be
Phạm Văn Nguyên <73846954+npv2k1@users.noreply.github.com>
2022-09-24 09:35:32 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Hello next</h1>
+      <h1>Hello, World!</h1>
     </div>
   )
 }
